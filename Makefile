4b6378a693d0c10e2e98a4bc4e3ac3b5cb74ac7b
Patrick Yeon <patrickyeon@gmail.com>
2017-05-29 20:51:50 -0700

    speed up the clock

--- Makefile
+++ Makefile
@@ -11,7 +11,7 @@ ARM_ARCH_FLAGS = -mthumb -mcpu=cortex-m0plus -msoft-float
 ARM_CFLAGS = -I$(OPENCM3_DIR)/include -DSTM32L0 $(ARM_ARCH_FLAGS)
 #ARM_CFLAGS += -fno-common -ffunction-sections -fdata-sections
 ARM_LDLIBS = -lopencm3_stm32l0
-ARM_LDSCRIPT = $(OPENCM3_DIR)/lib/stm32/l0/stm32l0xx8.ld
+ARM_LDSCRIPT = $(OPENCM3_DIR)/lib/stm32/l0/stm32l0xx4.ld
 ARM_LDFLAGS = -L$(OPENCM3_DIR)/lib --static -nostartfiles -T$(ARM_LDSCRIPT)
 ARM_LDFLAGS += $(ARM_ARCH_FLAGS)
 
@@ -26,4 +26,4 @@ stm:
 	$(ARM_PREFIX)$(OBJCOPY) -Oihex stm.elf stm.hex
 
 clean:
-	$(RM) *.o *.elf *.map *.bin
+	$(RM) -f *.d *.o *.elf *.map *.bin *.hex
--- stm.c
+++ stm.c
@@ -7,28 +7,32 @@
 #define FIXEDPIN GPIO3
 
 int main(void) {
+    // use the fastest internal clock
+    rcc_set_sysclk_source(RCC_HSI16);
+    rcc_osc_on(RCC_HSI16);
+
     rcc_periph_clock_enable(RCC_GPIOA);
     rcc_periph_clock_enable(RCC_GPIOB);
     gpio_mode_setup(GPIOA, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO15);
     gpio_mode_setup(GPIOB, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO5);
     gpio_clear(GPIOA, GPIO15);
     gpio_clear(GPIOB, GPIO5);
-    fneuron_t spiky_f;
+
     ineuron_t spiky_i;
-    RS_f(&spiky_f);
     RS_i(&spiky_i);
-    for (int i = 0; i < 5000; i++) {
-        if (i < 100) {
-            step_f(&spiky_f, 0, 0.1);
-            step_i(&spiky_i, 0, 10);
-        } else {
-            gpio_set(GPIOA, GPIO15);
-            step_f(&spiky_f, 10, 0.1);
-            gpio_clear(GPIOA, GPIO15);
-            
-            gpio_set(GPIOB, GPIO5);
-            step_i(&spiky_i, 10 * spiky_i.scale, 10);
-            gpio_clear(GPIOB, GPIO5);
-        }
+
+    for (int i = 0; i < 100; i++) {
+        step_i(&spiky_i, 0, 10);
+    }
+
+    while(1) {
+        // blip one pin so that I can calibrate out the time it takes to turn
+        //  a pin on and off from the loop timing later.
+        gpio_set(GPIOA, GPIO15);
+        gpio_clear(GPIOA, GPIO15);
+
+        gpio_set(GPIOB, GPIO5);
+        step_i(&spiky_i, 10 * spiky_i.scale, 10);
+        gpio_clear(GPIOB, GPIO5);
     }
 }
